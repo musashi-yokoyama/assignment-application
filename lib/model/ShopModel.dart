e42b61c8653917287b27d499a85914ed3bcb9451
yokoyama musashi <toya@yokoyamanoMacBook-Air.local>
2024-03-18 14:16:28 +0900

    ショップモデルの作成

---
+++ lib/model/ShopModel.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/widgets.dart';
+
+//・店舗名称,アクセス,サムネイル画像
+//・店舗名称,住所,営業時間,画像
+class Shop {
+  //コンストラクタ
+  Shop({
+    required this.name,
+    required this.address,
+    required this.station_name,
+    required this.access,
+    required this.open,
+    required this.logo_image,
+    required this.lat,
+    required this.lng,
+  });
+  //プロパティ
+  final String name;
+  final String address;
+  final String station_name;
+  final String access;
+  final String open;
+  final String logo_image;
+  final String lat;
+  final String lng;
+
+  // JSONからArticleを生成するファクトリコンストラクタ
+  factory Shop.fromJson(dynamic json) {
+    return Shop(
+      name: json['name'] as String,
+      address: json['address'] as String,
+      station_name: json['station_name'] as String,
+      access: json['access'] as String,
+      open: json['open'] as String,
+      logo_image: json['logo_image'] as String,
+      lat: json['lat'] as String,
+      lng: json['lng'] as String,
+    );
+  }
+}
