e7706461375a536242c23a955d6d57a74ecd1414
yokoyama musashi <toya@yokoyamanoMacBook-Air.local>
2024-03-20 09:39:24 +0900

    試し

--- lib/controller/MapController.dart
+++ lib/controller/MapController.dart
@@ -1,1 +1,32 @@
-//googlemapを監視するところ
+//googlemap for flutter
+//サンプルコード
+//一旦中心を三宮駅にしておく
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapWithMarker extends StatelessWidget {
+  final LatLng center;
+
+  const MapWithMarker({Key? key, required this.center}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      child: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: center,
+          zoom: 15,
+        ),
+        markers: {
+          Marker(
+            markerId: MarkerId('center_marker'),
+            position: center,
+            infoWindow: InfoWindow(title: '三宮駅'),
+          ),
+        },
+      ),
+    );
+  }
+}
