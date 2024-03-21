a69cfb019fbe8a420c6940583aa24b1260c47e7b
yokoyama musashi <toya@yokoyamanoMacBook-Air.local>
2024-03-21 10:22:50 +0900

    検索結果一覧ページひな形

---
+++ lib/view/ShopListPage.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../model/ShopModel.dart';
+import '../widget/ShopListWidget.dart';
+
+class ShopListPage extends StatefulWidget {
+  const ShopListPage({Key? key}) : super(key: key);
+
+  @override
+  State<ShopListPage> createState() => _ShopListPage();
+}
+
+class _ShopListPage extends State<ShopListPage> {
+  List<Map<String, dynamic>> shop = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('検索結果'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            //Listviewでスクロール可能なリストにする
+            child: ListView(
+
+              /*children: shop
+                  .map((shop) => ShopContainer(shop: shop))
+                  .toList(),*/
+
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
