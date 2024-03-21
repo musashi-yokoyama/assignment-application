185321d876a5138cbd5b4effad7e490bcc0d8fdd
yokoyama musashi <toya@yokoyamanoMacBook-Air.local>
2024-03-21 09:48:51 +0900

    格納できるようにした

--- lib/widget/ShopListWidget.dart
+++ lib/widget/ShopListWidget.dart
@@ -1,21 +1,76 @@
 import 'package:flutter/material.dart';
+
+import '../model/ShopModel.dart';
 //検索結果で表示する店舗情報を格納したwidget
 class ShopContainer extends StatelessWidget {
-  const ShopContainer({Key? key}) : super(key: key);
+  const ShopContainer({
+    super.key,
+    //shopモデルの情報を受け取る
+    required this .shop
+});
+
+  final Shop shop;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 20,
-        vertical: 16,
-      ),
+    //コンテナ同士の間に余白をつける
+    return Padding(
+        padding: const EdgeInsets.symmetric(
+        vertical: 12,
+        horizontal: 16,
+    ),
+    child: Container(
       decoration: const BoxDecoration(
         color: Colors.grey,
         borderRadius: BorderRadius.all(
           Radius.circular(32),
+          ),
         ),
-      ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+
+            //店名
+            Text(
+                shop.name,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  ),
+                ),
+
+            //アクセス
+            Text(
+              shop.access,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+
+            //サムネイル画像
+            Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    CircleAvatar(
+                      radius: 26,
+                      backgroundImage: NetworkImage(shop.logo_image),
+                    )
+                  ],
+                )
+              ],
+            )
+              ]
+            )
+          )
     );
   }
 }
