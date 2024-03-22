8a27752b1c887c072f2a7e43658ba77c41ee39ff
yokoyama musashi <toya@yokoyamanoMacBook-Air.local>
2024-03-22 16:35:46 +0900

    検索クエリ変更とエラーメッセージの実装

--- lib/controller/SearchShopController.dart
+++ lib/controller/SearchShopController.dart
@@ -4,10 +4,12 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import '../model/ShopModel.dart';
 
 //5つのクエリパラメータを設定しなければいけない？
-Future<List<Shop>> searchHotpapper(String keyword) async {
+Future<List<Shop>> searchHotpapper(String keyword, double lat, double lon, int range) async {
   //URL、クエリパラメータの設定
   final uri = Uri.https('webservice.recruit.co.jp', '/hotpepper/gourmet/v1/', {
-    'range': '3',
+    'lat': lat.toString(),
+    'lng': lon.toString(),
+    'range': range.toString(),
   });
 
   //アクセスキーの取得
@@ -20,9 +22,14 @@ Future<List<Shop>> searchHotpapper(String keyword) async {
   //レスポンスのハンドリング
   if (res.statusCode == 200) {
     // レスポンスをモデルクラスへ変換
-    final List<dynamic> body = jsonDecode(res.body);
-    return body.map((dynamic json) => Shop.fromJson(json)).toList();
+    final Map<String, dynamic> data = jsonDecode(res.body);
+    if (data.containsKey('results') && data['results'].containsKey('shop')) {
+      final List<dynamic> body = data['results']['shop'];
+      return body.map((dynamic json) => Shop.fromJson(json)).toList();
+    } else {
+      throw Exception('店舗が見つかりませんでした');
+    }
   } else {
-    return [];
+    throw Exception('失敗しました: ${res.statusCode}');
   }
 }
